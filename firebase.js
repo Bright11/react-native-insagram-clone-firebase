eeeab3a06afdbe35daef79b7e8dc2da72076f55b
Chika Nwazuo <34070274+Bright11@users.noreply.github.com>
2023-03-18 19:24:01 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 //import * as firebase from 'firebase'
 //import { initializeApp } from 'firebase/app';
 const firebaseConfig = {
-  apiKey: "AIzaSyBZtP9dlnqsNFqve8LvauyQsp3_dHC_flE",
-  authDomain: "react-native-instagram-f581b.firebaseapp.com",
-  projectId: "react-native-instagram-f581b",
-  storageBucket: "react-native-instagram-f581b.appspot.com",
-  messagingSenderId: "317629349120",
-  appId: "1:317629349120:web:701eab5d0afb5f4c69c7e5",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 // if (firebase.app.lenght === 0) {
 //     firebase.initializeApp(firebaseConfig);
